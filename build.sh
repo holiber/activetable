958edc0bb0ced177ff2ff401a7c9003f5d0d6266
alxgutnikov <alx.gutnikov@gmail.com>
2013-01-25 12:54:41 +0400

    fixed build.sh bug

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 VERSION=0.1
-w=src/activeTable/widgets
 
 #drop dist directory
 rm -fr dist
@@ -10,13 +9,15 @@ cat src/activeTable/data.js src/activeTable/table.js src/activeTable/table.tpl.j
 cat src/activeTable/table.css| java -jar yuicompressor-2.4.2.jar --type css >> dist/active-table-0.1.css
 
 #Widgets
-cat $w/actionButtons/actionButtons.js $w/actionButtons/actionButtons.tpl.js| java -jar yuicompressor-2.4.2.jar >> dist/active-table-$VERSION-widgets.js
+w='src/activeTable/widgets'
+cat $w/actionButtons/actionButtons.js $w/actionButtons/actionButtons.tpl.js| java -jar yuicompressor-2.4.2.jar --type js  >> dist/active-table-$VERSION-widgets.js
 cat $w/actionButtons/actionButtons.css | java -jar yuicompressor-2.4.2.jar --type css >> dist/active-table-0.1-widgets.css
-cat $w/perPage/perPage.js $w/perPage/perPage.tpl.js| java -jar yuicompressor-2.4.2.jar >> dist/active-table-$VERSION-widgets.js 
-cat $w/saveButtons/saveButtons.js $w/saveButtons/saveButtons.tpl.js| java -jar yuicompressor-2.4.2.jar >> dist/active-table-$VERSION-widgets.js 
+cat $w/perPage/perPage.js $w/perPage/perPage.tpl.js| java -jar yuicompressor-2.4.2.jar --type js >> dist/active-table-$VERSION-widgets.js 
+cat $w/saveButtons/saveButtons.js $w/saveButtons/saveButtons.tpl.js| java -jar yuicompressor-2.4.2.jar --type js >> dist/active-table-$VERSION-widgets.js 
 cat $w/saveButtons/saveButtons.css | java -jar yuicompressor-2.4.2.jar --type css >> dist/active-table-0.1-widgets.css
 
 #Images
 cp src/activeTable/icons.png dist/icons.png
 
 echo "Build done. see dist directory. See ya!"
+
