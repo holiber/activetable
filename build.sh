8268a8c7fb9ea9967ea479d0d719656c5858aea8
alxgutnikov <alx.gutnikov@gmail.com>
2013-01-28 11:27:02 +0400

    build script modified

--- build.sh
+++ build.sh
@@ -1,23 +1,46 @@
+# Usage: use ./build.sh to build a minified version of table and its widgets 
+# or ./build.sh --dev to build concatenated version
+
+#Increment active table version here
 VERSION=0.1
 
 #drop dist directory
 rm -fr dist
 mkdir dist
+p=`pwd`
 
-#Table base
-cat src/activeTable/data.js src/activeTable/table.js src/activeTable/table.tpl.js| java -jar yuicompressor-2.4.2.jar --type js >> dist/active-table-$VERSION.js
-cat src/activeTable/table.css| java -jar yuicompressor-2.4.2.jar --type css >> dist/active-table-0.1.css
+test()
+{
+	echo "Argument 1 is $1"
+}
 
-#Widgets
-w='src/activeTable/widgets'
-cat $w/actionButtons/actionButtons.js $w/actionButtons/actionButtons.tpl.js| java -jar yuicompressor-2.4.2.jar --type js  >> dist/active-table-$VERSION-widgets.js
-cat $w/actionButtons/actionButtons.css | java -jar yuicompressor-2.4.2.jar --type css >> dist/active-table-0.1-widgets.css
-cat $w/perPage/perPage.js $w/perPage/perPage.tpl.js| java -jar yuicompressor-2.4.2.jar --type js >> dist/active-table-$VERSION-widgets.js 
-cat $w/saveButtons/saveButtons.js $w/saveButtons/saveButtons.tpl.js| java -jar yuicompressor-2.4.2.jar --type js >> dist/active-table-$VERSION-widgets.js 
-cat $w/saveButtons/saveButtons.css | java -jar yuicompressor-2.4.2.jar --type css >> dist/active-table-0.1-widgets.css
+pack ()
+{
+	cd $p
+	cmd=$1
+	cmdcss=$2
+	todir=$3
+	SUFFIX=$4
+	mkdir -p dist/$todir
 
-#Images
-cp src/activeTable/icons.png dist/icons.png
+	#core table functions
+	cd src/activeTable
+	
+cat data.js table.js table.tpl.js | $cmd >> $p/dist/$todir/active-table-$VERSION$SUFFIX.js
+cat table.css | $cmdcss >> $p/dist/$todir/active-table-$VERSION$SUFFIX.css
+
+#table widgets
+cd widgets
+cat actionButtons/actionButtons.js actionButtons/actionButtons.tpl.js| $cmd >> $p/dist/$todir/active-table-widgets-$VERSION$SUFFIX.js
+cat actionButtons/actionButtons.css | $cmdcss >> $p/dist/$todir/active-table-widgets-$VERSION$SUFFIX.css
+cat perPage/perPage.js perPage/perPage.tpl.js| $cmd >> $p/dist/$todir/active-table-widgets-$VERSION$SUFFIX.js 
+cat saveButtons/saveButtons.js saveButtons/saveButtons.tpl.js| $cmdcss >> $p/dist/$todir/active-table-widgets-$VERSION$SUFFIX.js 
+cat saveButtons/saveButtons.css | $cmdcss >> $p/dist/$todir/active-table-widgets-$VERSION$SUFFIX.css
+cp $p/src/activeTable/icons.png $p/dist/$todir/icons.png
+}
+
+comp="java -jar $p/yuicompressor-2.4.2.jar"
+pack "$comp --type js" "$comp --type css" 'min' '.min'
+pack 'tee' 'tee' 'dev' ''
 
 echo "Build done. see dist directory. See ya!"
-
